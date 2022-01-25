b0cca7fea62d7da8ab818b63016d51428835f888
Thabo Gumede <76052777+Thabo-hub@users.noreply.github.com>
2022-01-25 10:52:18 +0200

    Adding functionality

--- js.js
+++ js.js
@@ -1,2 +1,51 @@
-alert("Hellow Thabo, Mash and Husna!");
+alert("Testing!");
+// variable
+var username = document.getElementById("name");
+var surname = document.getElementById("surname");
+var password = document.getElementById("password");
+
+const userLogin = () => {
+  // Create a user variable - does this need to be herer???
+  const user = {
+    id: 1,
+    name: username,
+    surname: surname,
+    password: password,
+  };
+
+  // check if user exists:
+  if (
+    localStorage.getItem(user.name) === username &&
+    localStorage.getItem(user.surname) === surname
+  ) {
+    alert("Welcome back");
+  } else {
+    alert("Please register");
+  }
+};
+
+const signUp = () => {
+  // Create a user variable
+  const user = {
+    id: 1,
+    name: document.getElementById("name"),
+    surname: document.getElementById("surname"),
+    password: document.getElementById("password"),
+  };
+  //   Check if uer exists:
+  if (
+    localStorage.getItem(user.name) === username &&
+    localStorage.getItem(user.surname) === surname
+  ) {
+    alert("User already exists");
+  } else {
+    // Creating a user if form validation is successful
+    if (typeof Storage !== "undefined") {
+      localStorage.setItem("user", JSON.stringify(user));
+      alert('Success! User data saved')
+    } else {
+      alert("we seem to have a problem");
+    }
+  }
+};
 
