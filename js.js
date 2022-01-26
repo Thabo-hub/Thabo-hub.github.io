28600c5dfe10b2b6441787fdf9b2116906c5d99b
Mashego Sililo <isthmussa@gmail.com>
2022-01-26 10:08:57 +0200

    fixing logn and register form bugs

--- js.js
+++ js.js
@@ -20,20 +20,20 @@ function goToLogin() {
 
 // Variables for handling Form Submits
 const loginForm = document.getElementById("login-form");
-const regForm = document.getElementById("reg-form");
 const username = document.getElementById("name");
 const surname = document.getElementById("surname");
 const password = document.getElementById("password");
 
 // I have set up a user in local storage for testing purposes
-const john = {
+const user = {
   name: "john",
   surname: "doe",
   password: "johndoe123",
 };
-localStorage.setItem("john", JSON.stringify(john));
+localStorage.setItem("john", JSON.stringify(user));
 
 // Handling Login
+
 loginForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -47,7 +47,7 @@ loginForm.addEventListener("submit", (e) => {
         obj.surname === surname.value &&
         obj.password === password.value
       ) {
-        alert("Welcome back");
+        alert(`Welcome back ${username.value}`);
       }
     } else {
       alert("Please register");
@@ -56,33 +56,36 @@ loginForm.addEventListener("submit", (e) => {
 });
 
 // Handling Registration
+const regForm = document.getElementById("reg-form");
+const regName = document.getElementById("reg-name");
+const regSurname = document.getElementById("reg-surname");
+const regPassword = document.getElementById("reg-password");
+
 regForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  if (username && surname && password) {
-    // check if user exists:
-    var checkUser = localStorage.getItem(username.value);
-    var obj = JSON.parse(checkUser);
-    if (obj) {
-      if (
-        obj.name === username.value &&
-        obj.surname === surname.value &&
-        obj.password === password.value
-      ) {
-        alert("Please return to Login");
-      }
-    } else {
-      // Create a user variable
-      if (username.value && surname.value && password.value) {
-        const user = {
-          name: username.value,
-          surname: surname.value,
-          password: password.value,
-        };
-        console.log(user);
-        //   Create new user
-        localStorage.setItem("user", JSON.stringify(user));
-        alert("user data saved");
-      }
+  // check if user exists:
+  var checkUser = localStorage.getItem(regName.value);
+  var obj = JSON.parse(checkUser);
+
+  if (obj) {
+    if (
+      obj.name === regName.value &&
+      obj.surname === regSurname.value &&
+      obj.password === regPassword.value
+    ) {
+      alert("This account already exists. Please return to Login");
+    }
+  } else {
+    // Create a user variable
+    if (regName.value && regSurname.value && regPassword.value) {
+      const newUser = {
+        name: regName.value,
+        surname: regSurname.value,
+        password: regPassword.value,
+      };
+      //   Create new user
+      localStorage.setItem(regName.value, JSON.stringify(newUser));
+      alert("Success! User data saved");
     }
   }
 });
